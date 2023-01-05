2cf03eb8035ef00a0f6f689150453a9675f1bffb
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-01-05 19:54:24 +0100

    fix(Tests): syntax error

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -14,10 +14,14 @@ def test_baseline_naive_model() -> None:
 
     splitter = ExpandingWindowSplitter(train_window_size=400, step=400)
     model = BaselineModel(strategy=BaselineStrategy.naive)
-    transformation = NoTransformation()
+    transformations = [NoTransformation()]
 
-    transformations_over_time = fit_transformations(X, y, splitter, transformation)
-    model_over_time = walk_forward_train(model, X, y, splitter, transformations_over_time)
+    transformations_over_time = fit_transformations(X, y, splitter, transformations)
+    model_over_time = walk_forward_train(
+        model, X, y, splitter, transformations_over_time
+    )
 
-    _, pred = walk_forward_inference(model_over_time, transformations_over_time, X, y, splitter)
+    _, pred = walk_forward_inference(
+        model_over_time, transformations_over_time, X, y, splitter
+    )
     assert (y[pred.index].shift(1) == pred).sum() == len(pred) - 1
