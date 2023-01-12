6076caeadc2cb699f612b79a3265ae2e32fbc0c2
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-01-12 19:10:10 +0100

    fix(Models): Baseline model fixed

--- src/drift/models/baseline.py
+++ src/drift/models/baseline.py
@@ -1,8 +1,9 @@
 from enum import Enum
 
 import numpy as np
+import pandas as pd
 
-from .base import Model, ModelType
+from .base import Model
 
 
 class BaselineStrategy(Enum):
@@ -16,37 +17,36 @@ class BaselineStrategy(Enum):
 class Baseline(Model):
 
     strategies = BaselineStrategy
-    type = ModelType.Univariate
 
     def __init__(self, strategy: BaselineStrategy, window_size: int = 100) -> None:
         self.strategy = strategy
         self.window_size = window_size
         self.name = f"BaselineModel-{strategy.value}"
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
         pass
 
-    def predict(self, X: np.ndarray) -> np.ndarray:
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
         if self.strategy == BaselineStrategy.sliding_mean:
             return np.array(
                 [
-                    np.mean(X[max(i - self.window_size, 0) : i + 1])
+                    np.mean(X.values[max(i - self.window_size, 0) : i + 1])
                     for i in range(len(X))
                 ]
             )
         elif self.strategy == BaselineStrategy.expanding_mean:
-            return np.array([np.mean(X[: i + 1]) for i in range(len(X))])
+            return np.array([np.mean(X.values[: i + 1]) for i in range(len(X))])
         elif self.strategy == BaselineStrategy.naive:
             return X
         elif self.strategy == BaselineStrategy.sliding_drift:
             return np.array(
                 [
-                    calculate_drift_predictions(X[max(i - self.window_size, 0) : i + 1])
+                    calculate_drift_predictions(X.values[max(i - self.window_size, 0) : i + 1])
                     for i in range(len(X))
                 ]
             )
         elif self.strategy == BaselineStrategy.expanding_drift:
-            return np.array([calculate_drift_predictions(X[: i + 1]) for i in len(X)])
+            return np.array([calculate_drift_predictions(X.values[: i + 1]) for i in len(X)])
         else:
             raise ValueError(f"Strategy {self.strategy} not implemented")
 
