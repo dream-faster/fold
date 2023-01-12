a5587755f986c13ba7ef90105c29e788bf275acb
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-01-12 18:29:37 +0100

    fix(Linter): suggestions

--- src/drift/models/base.py
+++ src/drift/models/base.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from enum import Enum
 
-import numpy as np
 import pandas as pd
 
 from ..transformations.base import Transformation
--- src/drift/models/sklearn.py
+++ src/drift/models/sklearn.py
@@ -1,11 +1,6 @@
-from typing import List
-
-import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator
 
 from drift.models.base import Model, ModelType
-from drift.transformations.base import Transformation
 
 
 class SKLearnModel(Model):
@@ -13,11 +8,11 @@ class SKLearnModel(Model):
     name = "SKLearnModel"
     type = ModelType.Multivariate
 
-    def __init__(self, model: BaseEstimator) -> None:
-        self.model = model
+    def __init__(self, model) -> None:
+        self.model = model.set_output(transform="pandas")
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
         self.model.fit(X, y)
 
-    def predict(self, X: np.ndarray) -> np.ndarray:
+    def predict(self, X: pd.DataFrame) -> pd.Series:
         return self.model.predict(X)
