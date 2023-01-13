d6b8ebd2bf629312d0a423f40f22bb9322ad6d6c
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-01-13 10:36:04 +0100

    chore(Tests): updated

--- tests/test_loop.py
+++ tests/test_loop.py
@@ -7,14 +7,15 @@ from drift.transformations import NoTransformation
 from tests.utils import generate_sine_wave_data
 
 
-def test_baseline_naive_model() -> None:
+def test_loop() -> None:
 
-    y = generate_sine_wave_data()
-    X = y.shift(1)
+    # the naive model returns X as prediction, so y.shift(1) should be == pred
+    X = generate_sine_wave_data()
+    y = y["sine"].shift(-1)
 
     splitter = ExpandingWindowSplitter(train_window_size=400, step=400)
-    transformations = [NoTransformation(), Baseline(strategy=BaselineStrategy.naive)]
+    transformations = [Baseline(strategy=BaselineStrategy.naive)]
 
     transformations_over_time = train(transformations, X, y, splitter)
     _, pred = infer(transformations_over_time, X, splitter)
-    assert (y[pred.index].shift(1) == pred).sum() == len(pred) - 1
+    assert (X[pred.index] == pred).sum() == len(pred) - 1
