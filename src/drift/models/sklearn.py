e23b75386267ad443cb0ee70b0a6aaf5d7591a4e
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-01-16 14:09:18 +0100

    fix(Models): wrap model output into pd.Series (#15)

--- src/drift/models/sklearn.py
+++ src/drift/models/sklearn.py
@@ -5,11 +5,11 @@ from drift.models.base import Model
 
 class SKLearnModel(Model):
     def __init__(self, model) -> None:
-        self.model = model.set_output(transform="pandas")
+        self.model = model
         self.name = model.__class__.__name__
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
         self.model.fit(X, y)
 
     def predict(self, X: pd.DataFrame) -> pd.Series:
-        return self.model.predict(X)
+        return pd.Series(data=self.model.predict(X).squeeze(), index=X.index)
