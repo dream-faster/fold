8f8f7de90c8b8c0dad9b5043b203caff016e2bf1
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-01-30 12:01:36 +0100

    fix(Transformations): added sklearn <1.2 compatibility (#39)

--- src/drift/transformations/sklearn.py
+++ src/drift/transformations/sklearn.py
@@ -5,27 +5,42 @@ from .base import FeatureSelector, Transformation
 
 class SKLearnTransformation(Transformation):
     def __init__(self, transformation) -> None:
-        self.transformation = transformation.set_output(transform="pandas")
-        self.name = transformation.__class__.__name__
+        if hasattr(transformation, "set_output"):
+            transformation = transformation.set_output(transform="pandas")
+        self.transformation = transformation
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
         self.transformation.fit(X, y)
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        return self.transformation.transform(X)
+        if hasattr(transformation, "set_output"):
+            return self.transformation.transform(X)
+        else:
+            return pd.DataFrame(
+                self.transformation.transform(X),
+                columns=self.transformation.get_feature_names_out(),
+            )
 
     def inverse_transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        return self.transformation.inverse_transform(X)
+        if hasattr(self.transformation, "set_output"):
+            return self.transformation.inverse_transform(X)
+        else:
+            return pd.DataFrame(
+                self.transformation.transform(X),
+                columns=self.transformation.get_feature_names_out(),
+            )
 
 
 class SKLearnFeatureSelector(FeatureSelector):
-    def __init__(self, model) -> None:
-        self.model = model.set_output(transform="pandas")
-        self.name = model.__class__.__name__
+    def __init__(self, transformation) -> None:
+        if hasattr(transformation, "set_output"):
+            transformation = transformation.set_output(transform="pandas")
+        self.transformation = transformation
+        self.name = transformation.__class__.__name__
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
-        self.model.fit(X, y)
-        self.selected_features = self.model.get_feature_names_out()
+        self.transformation.fit(X, y)
+        self.selected_features = self.transformation.get_feature_names_out()
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         return X[self.selected_features]
