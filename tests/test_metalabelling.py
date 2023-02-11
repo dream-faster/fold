1ae10463cc0f80520434b26292f9c01d42094391
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-02-11 16:10:48 +0100

    feature(Tests): first metalabeling test draft (fails)

--- tests/test_metalabelling.py
+++ tests/test_metalabelling.py
@@ -16,36 +16,30 @@ def test_metalabeling() -> None:
     X = generate_all_zeros(1000)
     y = X.shift(-1).squeeze()
 
-    # splitter = ExpandingWindowSplitter(train_window_size=400, step=400)
-    # transformations = [
-    #     Ensemble(
-    #         [
-    #             DummyClassifier(
-    #                 predicted_value=1,
-    #                 all_classes=[1, 0],
-    #                 predicted_probabilities=[1.0, 0.0],
-    #             ),
-    #             DummyClassifier(
-    #                 predicted_value=1,
-    #                 all_classes=[1, 0],
-    #                 predicted_probabilities=[0.5, 0.5],
-    #             ),
-    #             DummyClassifier(
-    #                 predicted_value=0,
-    #                 all_classes=[1, 0],
-    #                 predicted_probabilities=[0.0, 1.0],
-    #             ),
-    #         ]
-    #     ),
-    # ]
+    splitter = ExpandingWindowSplitter(train_window_size=400, step=400)
+    transformations = [
+        MetaLabeling(
+            primary=DummyClassifier(
+                    predicted_value=1,
+                    all_classes=[1, 0],
+                    predicted_probabilities=[1.0, 0.0],
+                ),
+            meta=DummyClassifier(
+                    predicted_value=0.5,
+                    all_classes=[1, 0],
+                    predicted_probabilities=[0.5, 0.5],
+                ),
+            positive_class=1
+        )
+    ]
 
-    # transformations_over_time = train(transformations, X, y, splitter)
-    # _, pred = backtest(transformations_over_time, X, y, splitter)
-    # assert (
-    #     pred["probabilities_Ensemble-DummyClassifier-DummyClassifier-DummyClassifier_1"]
-    #     == 0.5
-    # ).all()
-    # assert (
-    #     pred["probabilities_Ensemble-DummyClassifier-DummyClassifier-DummyClassifier_0"]
-    #     == 0.5
-    # ).all()
+    transformations_over_time = train(transformations, X, y, splitter)
+    _, pred = backtest(transformations_over_time, X, y, splitter)
+    assert (
+        pred["probabilities_Ensemble-DummyClassifier-DummyClassifier-DummyClassifier_1"]
+        == 0.5
+    ).all()
+    assert (
+        pred["probabilities_Ensemble-DummyClassifier-DummyClassifier-DummyClassifier_0"]
+        == 0.5
+    ).all()
