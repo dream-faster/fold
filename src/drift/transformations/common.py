6c940d4b2524232d2c5e67e437a1d8a351e0433f
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-02-24 10:20:03 +0100

    fix(Formatting): black applied

--- src/drift/transformations/common.py
+++ src/drift/transformations/common.py
@@ -11,9 +11,11 @@ def get_flat_list_of_transformations(
         if isinstance(transformations, List):
             return [get_all_transformations(t) for t in transformations]
         elif isinstance(transformations, Composite):
-            secondary_transformations = [] if (
-                transformations.get_child_transformations_secondary()
-            ) is None else transformations.get_child_transformations_secondary()
+            secondary_transformations = (
+                []
+                if (transformations.get_child_transformations_secondary()) is None
+                else transformations.get_child_transformations_secondary()
+            )
             return [
                 get_all_transformations(child_t)
                 for child_t in transformations.get_child_transformations_primary()
