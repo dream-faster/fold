a15adfbf0a0b3fcb572fbfb13bb8006cff9cb6d0
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-06 16:41:49 +0100

    feature(Tests): added half-finished tests

--- src/fold/models/__init__.py
+++ src/fold/models/__init__.py
@@ -3,4 +3,5 @@ from .baseline import BaselineNaive, BaselineNaiveSeasonal
 from .dummy import DummyClassifier, DummyRegressor
 from .ensemble import Ensemble, PerColumnEnsemble
 from .metalabeling import MetaLabeling
+from .select import SelectBest
 from .sklearn import SKLearnClassifier, SKLearnRegressor
