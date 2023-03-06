7deb4955051bc443f7568fdd478267a8db0c06a3
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-06 14:22:38 +0100

    fix(Tests): update syntax

--- tests/test_splitters.py
+++ tests/test_splitters.py
@@ -51,7 +51,7 @@ def test_sliding_window_splitter_embargo():
 def test_single_window_splitter():
 
     X = generate_sine_wave_data()
-    splitter = SingleWindowSplitter(proportion=0.4)
+    splitter = SingleWindowSplitter(train_window_size=0.4)
 
     splits = splitter.splits(len(X))
     assert len(splits) == 1
@@ -61,7 +61,7 @@ def test_single_window_splitter():
 def test_single_window_splitter_embargo():
 
     X = generate_sine_wave_data()
-    splitter = SingleWindowSplitter(proportion=0.4, embargo=10)
+    splitter = SingleWindowSplitter(train_window_size=0.4, embargo=10)
 
     splits = splitter.splits(len(X))
     assert len(splits) == 1
