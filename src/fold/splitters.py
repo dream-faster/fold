4aaf08aa400d11c7777e7642c94b5e074a74bbf0
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-06 15:43:04 +0100

    feature(Splitters): window_size can be either a float or an interger (#78)
    * feature(Splitters): window_size can be either a float or an interger
    
    * fix(Tests): update syntax
    
    * feature(Tests): added splitter tests

--- src/fold/splitters.py
+++ src/fold/splitters.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List, Optional, Union
 
 
 @dataclass
@@ -16,10 +16,21 @@ class Splitter:
         raise NotImplementedError
 
 
+def translate_float_if_needed(window_size: Union[int, float], length: int) -> int:
+    if window_size > 1 and type(window_size) is int:
+        return window_size
+    elif window_size < 1 and type(window_size) is float:
+        return int(window_size * length)
+    else:
+        raise ValueError(
+            "Invalid window size, should be either a float less than 1 or an int greater than 1"
+        )
+
+
 class SlidingWindowSplitter(Splitter):
     def __init__(
         self,
-        train_window_size: int,
+        train_window_size: Union[int, float],
         step: int,
         embargo: int = 0,
         start: int = 0,
@@ -33,23 +44,25 @@ class SlidingWindowSplitter(Splitter):
 
     def splits(self, length: int) -> List[Split]:
         end = self.end if self.end is not None else length
+        window_size = translate_float_if_needed(self.window_size, length)
+        step = translate_float_if_needed(self.step, length)
         return [
             Split(
                 model_index=index,
-                train_window_start=index - self.window_size,
+                train_window_start=index - window_size,
                 train_window_end=index - self.embargo,
                 test_window_start=index,
-                test_window_end=min(end, index + self.step),
+                test_window_end=min(end, index + step),
             )
-            for index in range(self.start + self.window_size, end, self.step)
+            for index in range(self.start + window_size, end, step)
         ]
 
 
 class ExpandingWindowSplitter(Splitter):
     def __init__(
         self,
-        train_window_size: int,
-        step: int,
+        train_window_size: Union[int, float],
+        step: Union[int, float],
         embargo: int = 0,
         start: int = 0,
         end: Optional[int] = None,
@@ -62,35 +75,39 @@ class ExpandingWindowSplitter(Splitter):
 
     def splits(self, length: int) -> List[Split]:
         end = self.end if self.end is not None else length
+        window_size = translate_float_if_needed(self.window_size, length)
+        step = translate_float_if_needed(self.step, length)
+
         return [
             Split(
                 model_index=index,
                 train_window_start=self.start,
                 train_window_end=index - self.embargo,
                 test_window_start=index,
-                test_window_end=min(end, index + self.step),
+                test_window_end=min(end, index + step),
             )
-            for index in range(self.start + self.window_size, end, self.step)
+            for index in range(self.start + window_size, end, step)
         ]
 
 
 class SingleWindowSplitter(Splitter):
     def __init__(
         self,
-        proportion: float,
+        train_window_size: Union[int, float],
         embargo: int = 0,
     ) -> None:
-        self.proportion = proportion
+        self.window_size = train_window_size
         self.embargo = embargo
 
     def splits(self, length: int) -> List[Split]:
-        index = int(length * self.proportion)
+        window_size = translate_float_if_needed(self.window_size, length)
+
         return [
             Split(
                 model_index=0,
                 train_window_start=0,
-                train_window_end=index - self.embargo,
-                test_window_start=index,
+                train_window_end=window_size - self.embargo,
+                test_window_start=window_size,
                 test_window_end=length,
             ),
         ]
