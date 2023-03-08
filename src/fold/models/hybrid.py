f46fa8f3957c736aa1669ffce8aa131b04562d57
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-08 12:08:00 +0100

    feature(Models): added Hybrid Composite

---
+++ src/fold/models/hybrid.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+from typing import Callable, List, Optional, Union
+
+import pandas as pd
+
+from fold.transformations.common import get_concatenated_names
+from fold.utils.checks import get_prediction_column
+
+from ..transformations.base import (
+    BlocksOrWrappable,
+    Composite,
+    TransformationsAlwaysList,
+)
+
+
+class Hybrid(Composite):
+
+    """
+    This is a composite transformation that takes a primary pipeline and a meta pipeline.
+    The primary pipeline is used to predict the target variable.
+    The meta pipeline is used to predict the primary pipeline's residual.
+    It adds together the primary pipeline's output with the predicted residual.
+
+    Also known as:
+    - Residual chasing
+    - ?
+
+    It's only applicable for regression tasks.
+    """
+
+    properties = Composite.Properties(
+        primary_requires_predictions=True,
+        primary_only_single_pipeline=True,
+        secondary_requires_predictions=True,
+        secondary_only_single_pipeline=True,
+    )
+
+    def __init__(
+        self,
+        primary: BlocksOrWrappable,
+        meta: BlocksOrWrappable,
+        positive_class: Union[int, float],
+        primary_output_included: bool = False,
+    ) -> None:
+        self.primary = [primary]
+        self.meta = [meta]
+        self.positive_class = positive_class
+        self.primary_output_included = primary_output_included
+        self.name = "Hybrid-" + get_concatenated_names(self.primary + self.meta)
+
+    def preprocess_X_secondary(
+        self, X: pd.DataFrame, results_primary: List[pd.DataFrame], index: int
+    ) -> pd.DataFrame:
+        if self.primary_output_included:
+            return pd.concat([X] + results_primary, axis="columns")
+        else:
+            return X
+
+    def preprocess_y_secondary(
+        self, y: pd.Series, results_primary: List[pd.DataFrame]
+    ) -> pd.Series:
+        predictions = get_prediction_column(results_primary[0])
+        return y - predictions
+
+    def postprocess_result_primary(self, results: List[pd.DataFrame]) -> pd.DataFrame:
+        raise NotImplementedError
+
+    def postprocess_result_secondary(
+        self,
+        primary_results: List[pd.DataFrame],
+        secondary_results: List[pd.DataFrame],
+    ) -> pd.DataFrame:
+        primary_predictions = get_prediction_column(primary_results[0])
+        residual_predictions = get_prediction_column(secondary_results[0])
+
+        result = (
+            primary_predictions + meta_probabilities_positive_class.squeeze()
+        ).rename(f"predictions_{self.name}")
+        dc = {
+            col: f"probabilities_{self.name}_" + col.split("_")[-1]
+            for col in meta_probabilities.columns
+        }
+        meta_probabilities = meta_probabilities.rename(columns=dc)
+        return pd.concat([result, meta_probabilities], axis="columns")
+
+    def get_child_transformations_primary(self) -> TransformationsAlwaysList:
+        return self.primary
+
+    def get_child_transformations_secondary(
+        self,
+    ) -> Optional[TransformationsAlwaysList]:
+        return self.meta
+
+    def clone(self, clone_child_transformations: Callable) -> MetaLabeling:
+        return MetaLabeling(
+            primary=clone_child_transformations(self.primary),
+            meta=clone_child_transformations(self.meta),
+            positive_class=self.positive_class,
+            primary_output_included=self.primary_output_included,
+        )
