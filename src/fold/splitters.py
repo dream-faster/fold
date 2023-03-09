7430611c89bd40ad87c6fc3e6998b3dd0c77cab3
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-09 12:38:10 +0100

    fix(Splitters): allow window_size of `1`

--- src/fold/splitters.py
+++ src/fold/splitters.py
@@ -18,7 +18,7 @@ class Splitter:
 
 
 def translate_float_if_needed(window_size: Union[int, float], length: int) -> int:
-    if window_size > 1 and type(window_size) is int:
+    if window_size >= 1 and type(window_size) is int:
         return window_size
     elif window_size < 1 and type(window_size) is float:
         return int(window_size * length)
