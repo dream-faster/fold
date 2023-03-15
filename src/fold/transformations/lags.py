3b39617b07426f857a4fb6ca24ab93d3335b6014
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-15 12:53:07 +0100

    feature(Transformation): added `AddLagsY` class

--- src/fold/transformations/lags.py
+++ src/fold/transformations/lags.py
@@ -1,39 +1,50 @@
-# from typing import List, Optional, Union
+from typing import List, Optional, Union
 
-# import pandas as pd
+import pandas as pd
 
-# from ..utils.list import wrap_in_list
-# from .base import Transformation
+from ..utils.list import wrap_in_list
+from .base import Transformation
 
-# class AddLagsY(Transformation):
-#     properties = Transformation.Properties(mode=Transformation.Properties.Mode.online)
 
-#     def __init__(self, lags: Union[List[int], int]) -> None:
-#         self.lags = wrap_in_list(lags)
-#         self.max_lag = max(self.lags)
-#         self.name = f"AddLagsY-{self.lags}"
+class AddLagsY(Transformation):
+    properties = Transformation.Properties()
 
-#     def fit(
-#         self,
-#         X: pd.DataFrame,
-#         y: Optional[pd.Series],
-#         sample_weights: Optional[pd.Series] = None,
-#     ) -> None:
-#         self.y_in_sample = y.copy()
-#         self.past_y = y[: -self.max_lag].copy()
+    def __init__(self, lags: Union[List[int], int]) -> None:
+        self.lags = wrap_in_list(lags)
+        self.max_lag = max(self.lags)
+        self.name = f"AddLagsY-{self.lags}"
 
-#     def update(
-#         self,
-#         X: pd.DataFrame,
-#         y: Optional[pd.Series],
-#         sample_weights: Optional[pd.Series] = None,
-#     ) -> None:
-#         self.past_y[y.index[0]] = y.squeeze()
-#         self.past_y = pd.concat([self.past_y, y], axis="index")[: -self.max_lag]
+    def fit(
+        self,
+        X: pd.DataFrame,
+        y: Optional[pd.Series],
+        sample_weights: Optional[pd.Series] = None,
+    ) -> None:
+        self.y_in_sample = y.copy()
+        self.past_y = y[-self.max_lag :].copy()
 
-#     def transform(self, X: pd.DataFrame, in_sample: bool) -> pd.DataFrame:
-#         X = X.copy()
-#         y = self.y_in_sample if in_sample else self.past_y
-#         for lag in self.lags:
-#             X[f"y_lag_{lag}"] = y.shift(lag)[-len(X) :]
-#         return X
+    def update(
+        self,
+        X: pd.DataFrame,
+        y: Optional[pd.Series],
+        sample_weights: Optional[pd.Series] = None,
+    ) -> None:
+        if len(y) == 1:
+            self.past_y[y.index[0]] = y.squeeze()
+        else:
+            self.past_y = pd.concat([self.past_y, y], axis="index")
+        length_to_keep = len(X) + self.max_lag
+        self.past_y = self.past_y[-length_to_keep:]
+
+    def transform(self, X: pd.DataFrame, in_sample: bool) -> pd.DataFrame:
+        X = X.copy()
+        if len(X) > 1:
+            y = self.y_in_sample if in_sample else self.past_y
+            for lag in self.lags:
+                X[f"y_lag_{lag}"] = y.shift(lag)[-len(X) :]
+        else:
+            y = self.y_in_sample if in_sample else self.past_y
+            for lag in self.lags:
+                X[f"y_lag_{lag}"] = y[-lag]
+
+        return X
