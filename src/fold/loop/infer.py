a2eff555c7ee0b6d9ad7fd4dd2336d91105adf2c
Daniel Szemerey <account@szemerey.eu>
2023-03-16 15:52:39 +0100

    feature: Added handling of X as None for `infer` and `update` aswell.

--- src/fold/loop/infer.py
+++ src/fold/loop/infer.py
@@ -10,15 +10,18 @@ from .types import Backend, Stage
 
 def infer(
     transformations: DeployableTransformations,
-    X: pd.DataFrame,
+    X: Optional[pd.DataFrame],
 ) -> OutOfSamplePredictions:
     """
     Run inference on a set of Transformations and given data.
     Does not mutate or change the transformations in any way.
     A follow-up call to `update` is required to update the transformations, when the ground truth is available.
+    If X is None it will predict one step ahead.
     """
-
-    assert type(X) is pd.DataFrame, "X must be a pandas DataFrame."
+    if X is None:
+        X = pd.DataFrame([0], columns=[0])
+    else:
+        assert type(X) is pd.DataFrame, "X must be a pandas DataFrame."
 
     results = recursively_transform(
         X, None, None, transformations, stage=Stage.infer, backend=Backend.no
@@ -28,7 +31,7 @@ def infer(
 
 def update(
     transformations: DeployableTransformations,
-    X: pd.DataFrame,
+    X: Optional[pd.DataFrame],
     y: pd.Series,
     sample_weights: Optional[pd.Series] = None,
 ) -> DeployableTransformations:
@@ -36,8 +39,10 @@ def update(
     Update a set of Transformations with new data.
     Returns a new set of Transformations, does not mutate the original.
     """
-
-    assert type(X) is pd.DataFrame, "X must be a pandas DataFrame."
+    if X is None:
+        X = pd.DataFrame(0, index=y.index, columns=[0])
+    else:
+        assert type(X) is pd.DataFrame, "X must be a pandas DataFrame."
     assert type(y) is pd.Series, "y must be a pandas Series."
 
     transformations = deepcopy_transformations(transformations)
