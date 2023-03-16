7b3f45e4b3156e839a3b3fcc1e56a80545799257
Daniel Szemerey <account@szemerey.eu>
2023-03-16 11:24:10 +0100

    chore: Flake

--- examples/sktime_autoarima_airline.py
+++ examples/sktime_autoarima_airline.py
@@ -1,20 +1,18 @@
 import numpy as np
 import pandas as pd
-from fold.loop import backtest, train
-from fold.splitters import ExpandingWindowSplitter
+from fold_models.sktime import WrapSktime
 from krisi import score
 from sktime.datasets import load_airline
 from sktime.forecasting.arima import AutoARIMA
 
-
-from fold_models.sktime import WrapSktime
+from fold.loop import backtest, train
+from fold.splitters import ExpandingWindowSplitter
 
 y = load_airline()
 splitter = ExpandingWindowSplitter(initial_train_window=50, step=1)
 X = pd.DataFrame(np.zeros(len(y)), index=y.index)
 
 model = WrapSktime.from_model(AutoARIMA(), use_exogenous=False)
-
 
 transformations_over_time = train(model, X, y, splitter)
 pred = backtest(transformations_over_time, X, y, splitter)
