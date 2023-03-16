3c51382412b39116229cda4ca62b5c93612487fd
Daniel Szemerey <account@szemerey.eu>
2023-03-16 11:20:54 +0100

    feature: Added Sktime autoarima.

--- examples/sktime_autoarima_airline.py
+++ examples/sktime_autoarima_airline.py
@@ -1,17 +1,17 @@
 import numpy as np
 import pandas as pd
 from fold.loop import backtest, train
-from fold.splitters import ExpandingWindowSplitter, SingleWindowSplitter
+from fold.splitters import ExpandingWindowSplitter
 from krisi import score
 from sktime.datasets import load_airline
-from sktime.forecasting.arima import ARIMA, AutoARIMA
-from sktime.forecasting.naive import NaiveForecaster
+from sktime.forecasting.arima import AutoARIMA
+
 
 from fold_models.sktime import WrapSktime
 
 y = load_airline()
 splitter = ExpandingWindowSplitter(initial_train_window=50, step=1)
-X = pd.DataFrame(np.zeros(len(y)))
+X = pd.DataFrame(np.zeros(len(y)), index=y.index)
 
 model = WrapSktime.from_model(AutoARIMA(), use_exogenous=False)
 
@@ -19,4 +19,4 @@ model = WrapSktime.from_model(AutoARIMA(), use_exogenous=False)
 transformations_over_time = train(model, X, y, splitter)
 pred = backtest(transformations_over_time, X, y, splitter)
 
-score(y, pred)
+score(y[pred.index], pred.iloc[:, 0]).print_summary()
