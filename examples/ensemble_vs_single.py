415896baa0da68805fac6072fb1861e9d8220813
Daniel Szemerey <account@szemerey.eu>
2023-03-20 18:25:55 +0100

    feature: Updated the example and created the comparison.

--- examples/ensemble_vs_single.py
+++ examples/ensemble_vs_single.py
@@ -7,12 +7,13 @@
 # All of the pipeline are tested on the same data: Let's forecast the residual load in the network of the Bundesland of Hessen.
 
 from fold_models.statsforecast import WrapStatsForecast
-from krisi import score
+from krisi import compare, score
 from statsforecast.models import ARIMA
 
 # %%
 from utils import load_dataset
 
+from fold.composites.ensemble import Ensemble
 from fold.loop import backtest, train
 from fold.splitters import ExpandingWindowSplitter
 
@@ -20,13 +21,22 @@ from fold.splitters import ExpandingWindowSplitter
 # Let's load in the data.
 # 1. Some indexes are duplicate (due to summer-time/winter-time change).
 #   We deduplicate those rows and only keep the first instance of each duplicate.
-# 2. Then we take out our target column (`y`) and pop it from our Exogenous DataFrame (`X``)
+# 2. Then we take out our target column (`y`) and pop it from our Exogenous DataFrame (`X`)
 
 # %%
 target = "residual_load"
 X = load_dataset("energy/industrial_pv_load").set_index("time", drop=True)
 X = X[~X.index.duplicated(keep="first")]
 y = X.pop(target)
+
+
+# %% [markdown]
+# Let's first define a Splitter that allow us to run the same Continous Validation on all three pipelines
+# We also define a `scorecards` list where we collect the results of all three evalutations.
+
+# %%
+splitter = ExpandingWindowSplitter(initial_train_window=0.15, step=0.1)
+scorecards = []
 
 # %% [markdown]
 ### Pipeline 1 - `ARIMA` with order `(1, 1, 0)`
@@ -36,14 +46,14 @@ y = X.pop(target)
 pipeline_arima_1 = WrapStatsForecast(ARIMA, {"order": (1, 1, 0)}, use_exogenous=False)
 
 ## Training the model
-splitter = ExpandingWindowSplitter(initial_train_window=0.15, step=0.1)
-transformations_over_time = train(pipeline_arima_1, X, y, splitter)
+transformations_over_time = train(pipeline_arima_1.clone(), X, y, splitter)
 
 ## Evaluating the model
 pred = backtest(transformations_over_time, X, y, splitter)
 
-sc = score(y[pred.index], pred.squeeze(), "Pipeline_ARIMA_1").save()
+sc = score(y[pred.index], pred.squeeze(), "Pipeline_ARIMA_1")
 sc.print_summary(extended=False)
+scorecards.append(sc)
 
 # %% [markdown]
 ### Pipeline 1 - `ARIMA` with order `(2, 1, 1)`
@@ -53,28 +63,34 @@ sc.print_summary(extended=False)
 pipeline_arima_2 = WrapStatsForecast(ARIMA, {"order": (2, 1, 1)}, use_exogenous=False)
 
 ## Training the model
-splitter = ExpandingWindowSplitter(initial_train_window=0.15, step=0.1)
-transformations_over_time = train(pipeline_arima_2, X, y, splitter)
+transformations_over_time = train(pipeline_arima_2.clone(), X, y, splitter)
 
 ## Evaluating the model
 pred = backtest(transformations_over_time, X, y, splitter)
 
-sc = score(y[pred.index], pred.squeeze(), "Pipeline_ARIMA_2").save()
+sc = score(y[pred.index], pred.squeeze(), "Pipeline_ARIMA_2")
 sc.print_summary(extended=False)
+scorecards.append(sc)
 
 # %% [markdown]
 ### Composite Pipeline - Both previous ARIMAs Ensembled
 # Let's create another simple ARIMA model with Statforecast wrapped with fold-models
 
 # %%
-pipeline_ensemble = WrapStatsForecast(ARIMA, {"order": (2, 1, 1)}, use_exogenous=False)
+pipeline_ensemble = Ensemble([pipeline_arima_1.clone(), pipeline_arima_2.clone()])
 
 ## Training the model
-splitter = ExpandingWindowSplitter(initial_train_window=0.15, step=0.1)
-transformations_over_time = train(pipeline_arima_2, X, y, splitter)
+transformations_over_time = train(pipeline_ensemble, X, y, splitter)
 
 ## Evaluating the model
 pred = backtest(transformations_over_time, X, y, splitter)
 
-sc = score(y[pred.index], pred.squeeze(), "Pipeline_ARIMA_2").save()
+sc = score(y[pred.index], pred.squeeze(), "Pipeline_Ensemble")
 sc.print_summary(extended=False)
+scorecards.append(sc)
+
+# %% [markdown]
+### Comparing the results
+
+# %%
+compare(scorecards)
