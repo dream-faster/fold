8ea34478aa1a00c128b40b7f0ae52c7823b6afd7
Daniel Szemerey <account@szemerey.eu>
2023-03-21 14:16:49 +0100

    feature: Added new types for holiday feature adding.

--- src/fold/transformations/features.py
+++ src/fold/transformations/features.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import List, Union
 
 import holidays
@@ -5,6 +6,40 @@ import pandas as pd
 
 from ..utils.list import wrap_in_list
 from .base import Transformation, fit_noop
+
+
+def get_weekends(dates: pd.DatetimeIndex) -> pd.Series:
+    return pd.Series((dates.weekday > 4).astype(int), index=dates)
+
+
+def get_holidays(dates: pd.DatetimeIndex, country_codes: List[str]) -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            country_code: dates.isin(
+                holidays.country_holidays(
+                    country_code, years=range(dates[0].year, dates[-1].year)
+                )
+            ).astype(int)
+            for country_code in country_codes
+        },
+        index=dates,
+    )
+
+
+class HolidayTypes(Enum):
+    holiday_binary = "holiday_binary"
+    holiday_weekend = "holiday_weekend"
+    holidays_differentiated = "holidays_differentiated"
+
+    @staticmethod
+    def from_str(value: Union[str, "HolidayTypes"]) -> "HolidayTypes":
+        if isinstance(value, HolidayTypes):
+            return value
+        for strategy in HolidayTypes:
+            if strategy.value == value:
+                return strategy
+        else:
+            raise ValueError(f"Unknown HolidayType: {value}")
 
 
 class AddHolidayFeatures(Transformation):
@@ -15,30 +50,28 @@ class AddHolidayFeatures(Transformation):
 
     properties = Transformation.Properties()
 
-    def __init__(self, country_codes: Union[List[str], str]) -> None:
+    def __init__(
+        self,
+        country_codes: Union[List[str], str],
+        type: HolidayTypes = HolidayTypes.holiday_weekend,
+    ) -> None:
         self.country_codes = wrap_in_list(country_codes)
         self.name = f"AddHolidayFeatures-{self.country_codes}"
-        self.holidays = [
-            holidays.country_holidays(country_code)
-            for country_code in self.country_codes
-        ]
+        self.type = HolidayTypes.from_str(type)
 
     def transform(self, X: pd.DataFrame, in_sample: bool) -> pd.DataFrame:
-        return pd.concat(
-            [
-                X,
-                pd.DataFrame(
-                    {
-                        country.country: X.index.to_series()
-                        .map(lambda d: d in country)
-                        .to_list()
-                        for country in self.holidays
-                    },
-                    index=X.index,
-                ),
-            ],
-            axis="columns",
-        )
+        if self.type is HolidayTypes.holiday_binary:
+            return get_holidays(X.index, self.country_codes)
+
+        elif self.type is HolidayTypes.holiday_weekend:
+            return (
+                get_holidays(X.index, self.country_codes)
+                .mul(2)
+                .add(get_weekends(X.index), axis="index")
+            )
+
+        elif self.type is HolidayTypes.holidays_differentiated:
+            return get_holidays(X.index, self.country_codes) + get_weekends(X.index)
 
     fit = fit_noop
     update = fit_noop
