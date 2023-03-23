8e04c689a2dded9113ef5bfb9eeac58cfb2e9f6c
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-23 09:41:29 +0100

    refactor(Loop): broke up a large function into smaller pieces, introduced `BackendDependentFunctions` type

---
+++ src/fold/loop/backend/__init__.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from typing import Callable
+
+from fold.loop.types import Backend
+
+
+@dataclass
+class BackendDependentFunctions:
+    process_primary_child_transformations: Callable
+    process_secondary_child_transformations: Callable
+    train_transformations: Callable
+
+
+def get_backend_dependent_functions(backend: Backend) -> BackendDependentFunctions:
+    if backend == Backend.ray:
+        from .ray import (
+            process_primary_child_transformations,
+            process_secondary_child_transformations,
+            train_transformations,
+        )
+
+        return BackendDependentFunctions(
+            process_primary_child_transformations,
+            process_secondary_child_transformations,
+            train_transformations,
+        )
+    elif backend == Backend.no:
+        from .sequential import (
+            process_primary_child_transformations,
+            process_secondary_child_transformations,
+            train_transformations,
+        )
+
+        return BackendDependentFunctions(
+            process_primary_child_transformations,
+            process_secondary_child_transformations,
+            train_transformations,
+        )
