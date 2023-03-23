7be808de61e2de05104334fcce0f8ce8e9f427b3
Daniel Szemerey <account@szemerey.eu>
2023-03-23 14:38:16 +0100

    fix: Imports.

--- src/fold/__init__.py
+++ src/fold/__init__.py
@@ -1,6 +1,6 @@
 from .loop.backtesting import backtest
-from .loop.encase import backtest_evaluate as evaluate
-from .loop.encase import train_backtest_evaluate as train_evuate
+from .loop.encase import backtest_score as evaluate
+from .loop.encase import train_backtest_score as train_evaluate
 from .loop.inference import infer, update
 from .loop.training import train, train_for_deployment
 from .loop.types import Backend, TrainMethod
--- src/fold/loop/encase.py
+++ src/fold/loop/encase.py
@@ -1,18 +1,18 @@
 from typing import TYPE_CHECKING, Tuple
 
 import pandas as pd
-from backtesting import backtest
-from splitters import Splitter
-from training import train
-from transformations.base import BlocksOrWrappable
 
 from ..all_types import OutOfSamplePredictions, TransformationsOverTime
+from ..splitters import Splitter
+from ..transformations.base import BlocksOrWrappable
+from .backtesting import backtest
+from .training import train
 
 if TYPE_CHECKING:
     from krisi import ScoreCard
 
 
-def backtest_evaluate(
+def backtest_score(
     transformations_over_time: TransformationsOverTime,
     X: pd.DataFrame,
     y: pd.Series,
@@ -26,7 +26,7 @@ def backtest_evaluate(
     return scorecard, pred
 
 
-def train_backtest_evaluate(
+def train_backtest_score(
     transformations: BlocksOrWrappable,
     X: pd.DataFrame,
     y: pd.Series,
@@ -34,6 +34,6 @@ def train_backtest_evaluate(
 ) -> Tuple["ScoreCard", OutOfSamplePredictions, TransformationsOverTime]:
     transformations_over_time = train(transformations, X, y, splitter)
 
-    scorecard, pred = backtest_evaluate(transformations_over_time, X, y, splitter)
+    scorecard, pred = backtest_score(transformations_over_time, X, y, splitter)
 
     return scorecard, pred, transformations_over_time
