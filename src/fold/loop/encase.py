90d10c035f88af3e1d02e2d17249d15440cd104b
Daniel Szemerey <account@szemerey.eu>
2023-03-24 15:19:08 +0100

    feature(Encase): Updated abstractions with more arguments.

--- src/fold/loop/encase.py
+++ src/fold/loop/encase.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Callable, Dict, Tuple, Union
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Tuple, Union
 
 import pandas as pd
 from sklearn.metrics import mean_squared_error
@@ -8,6 +8,7 @@ from ..splitters import ExpandingWindowSplitter, Splitter
 from ..transformations.base import BlocksOrWrappable
 from .backtesting import backtest
 from .training import train
+from .types import Backend, TrainMethod
 
 if TYPE_CHECKING:
     from krisi import ScoreCard
@@ -18,14 +19,32 @@ def backtest_score(
     X: pd.DataFrame,
     y: pd.Series,
     splitter: Splitter,
+    backend: Backend = Backend.no,
+    sample_weights: Optional[pd.Series] = None,
+    silent: bool = False,
+    mutate: bool = False,
     with_krisi: bool = False,
+    krisi_args: Optional[Dict[str, Any]] = None,
     evaluation_func: Callable = mean_squared_error,
 ) -> Tuple[Union["ScoreCard", Dict[str, float]], OutOfSamplePredictions]:
-    pred = backtest(transformations_over_time, X, y, splitter)
+    pred = backtest(
+        transformations_over_time,
+        X,
+        y,
+        splitter,
+        backend,
+        sample_weights,
+        silent,
+        mutate,
+    )
     if with_krisi:
         from krisi import score
 
-        scorecard = score(y[pred.index], pred.squeeze())
+        scorecard = score(
+            y[pred.index],
+            pred.squeeze(),
+            **(krisi_args if krisi_args is not None else {}),
+        )
     else:
         scorecard = {
             "mean_squared_error": evaluation_func(y[pred.index], pred.squeeze())
@@ -38,16 +57,35 @@ def train_backtest_score(
     X: pd.DataFrame,
     y: pd.Series,
     splitter: Splitter = ExpandingWindowSplitter(initial_train_window=0.2, step=0.2),
+    backend: Backend = Backend.no,
+    sample_weights: Optional[pd.Series] = None,
+    train_method: TrainMethod = TrainMethod.parallel,
+    silent: bool = False,
+    mutate: bool = False,
     with_krisi: bool = False,
+    krisi_args: Optional[Dict[str, Any]] = None,
+    evaluation_func: Callable = mean_squared_error,
 ) -> Tuple[
     Union["ScoreCard", Dict[str, float]],
     OutOfSamplePredictions,
     TransformationsOverTime,
 ]:
-    transformations_over_time = train(transformations, X, y, splitter)
+    transformations_over_time = train(
+        transformations, X, y, splitter, sample_weights, train_method, backend, silent
+    )
 
     scorecard, pred = backtest_score(
-        transformations_over_time, X, y, splitter, with_krisi
+        transformations_over_time,
+        X,
+        y,
+        splitter,
+        backend,
+        sample_weights,
+        silent,
+        mutate,
+        with_krisi,
+        krisi_args,
+        evaluation_func,
     )
 
     return scorecard, pred, transformations_over_time
