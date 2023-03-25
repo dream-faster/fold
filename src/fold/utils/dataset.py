362a6655b919ecc757279447907fa6b6baa4b510
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-25 14:05:04 +0100

    fix(Utils): made it compatible with py3.7

--- src/fold/utils/dataset.py
+++ src/fold/utils/dataset.py
@@ -1,4 +1,7 @@
-from typing import Literal, Optional, Tuple
+from __future__ import annotations
+
+from enum import Enum
+from typing import Optional, Tuple, Union
 
 import pandas as pd
 
@@ -10,14 +13,33 @@ def load_dataset(
     return pd.read_csv(f"{base_path}/{dataset_name}.csv", parse_dates=True, index_col=0)
 
 
+class DeduplicationStrategy(Enum):
+    first = "first"
+    last = "last"
+
+    @staticmethod
+    def from_str(value: Union[str, DeduplicationStrategy]) -> DeduplicationStrategy:
+        if isinstance(value, DeduplicationStrategy):
+            return value
+        for strategy in DeduplicationStrategy:
+            if strategy.value == value:
+                return strategy
+        else:
+            raise ValueError(f"Unknown DeduplicationStrategy: {value}")
+
+
 def process_dataset(
     df: pd.DataFrame,
     target_col: str,
-    deduplicate_strategy: Optional[Literal["first", "last", False]] = None,
+    deduplication_strategy: Optional[Union[DeduplicationStrategy, str]] = None,
     shorten: Optional[int] = None,
 ) -> Tuple[pd.DataFrame, pd.Series]:
-    if deduplicate_strategy is not None:
-        df = df[~df.index.duplicated(keep=deduplicate_strategy)]
+    if deduplication_strategy is not None:
+        df = df[
+            ~df.index.duplicated(
+                keep=DeduplicationStrategy.from_str(deduplication_strategy)
+            )
+        ]
     if shorten is not None:
         df = df[:shorten]
     y = df[target_col].shift(-1)[:-1]
@@ -30,12 +52,12 @@ def get_preprocessed_dataset(
     dataset_name: str,
     target_col: str,
     base_path: str = "https://raw.githubusercontent.com/dream-faster/datasets/main/datasets",
-    deduplicate_strategy: Optional[Literal["first", "last", False]] = None,
+    deduplication_strategy: Optional[Union[DeduplicationStrategy, str]] = None,
     shorten: Optional[int] = None,
 ) -> Tuple[pd.DataFrame, pd.Series]:
     return process_dataset(
         load_dataset(dataset_name, base_path),
         target_col=target_col,
-        deduplicate_strategy=deduplicate_strategy,
+        deduplication_strategy=deduplication_strategy,
         shorten=shorten,
     )
