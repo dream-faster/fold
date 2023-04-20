ccc7fc9de7fc2bafb9372278e75d948e7a35cffe
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-04-20 14:07:31 +0200

    refactor(Tests): use built-in pytest decorator to test the permutation of parameters

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,13 +1,15 @@
+import pytest
 from sklearn.ensemble import HistGradientBoostingRegressor
 
-from fold.loop import Backend, train_evaluate
+from fold.loop import train_evaluate
 from fold.loop.encase import train_backtest
 from fold.splitters import ExpandingWindowSplitter
 from fold.transformations.lags import AddLagsX, AddLagsY
 from fold.utils.dataset import get_preprocessed_dataset
 
 
-def test_on_weather_data() -> None:
+@pytest.mark.parametrize("backend", ["no", "ray"])
+def test_on_weather_data(backend: str) -> None:
     X, y = get_preprocessed_dataset(
         "weather/historical_hourly_la",
         target_col="temperature",
@@ -20,26 +22,7 @@ def test_on_weather_data() -> None:
         HistGradientBoostingRegressor(),
     ]
 
-    _, _ = train_backtest(pipeline, X, y, splitter)
-
-
-def test_on_weather_data_ray() -> None:
-    import ray
-
-    ray.init(ignore_reinit_error=True)
-    X, y = get_preprocessed_dataset(
-        "weather/historical_hourly_la",
-        target_col="temperature",
-        shorten=1000,
-    )
-    splitter = ExpandingWindowSplitter(initial_train_window=0.2, step=0.2)
-    pipeline = [
-        AddLagsX(columns_and_lags=[("pressure", list(range(1, 3)))]),
-        AddLagsY(list(range(1, 10))),
-        HistGradientBoostingRegressor(),
-    ]
-
-    _, _ = train_backtest(pipeline, X, y, splitter, backend=Backend.ray)
+    _, _ = train_backtest(pipeline, X, y, splitter, backend=backend)
 
 
 def test_train_evaluate() -> None:
