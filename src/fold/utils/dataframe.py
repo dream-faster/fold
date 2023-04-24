a3c86883fcbd8cbed3126ddb34fc44af6cc327ca
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-04-24 17:35:00 +0200

    fix(Utils): `to_series` should return a series if dataframe has a single column and value, tests added

--- src/fold/utils/dataframe.py
+++ src/fold/utils/dataframe.py
@@ -5,8 +5,12 @@ import pandas as pd
 
 def to_series(dataframe_or_series: Union[pd.DataFrame, pd.Series]) -> pd.Series:
     if isinstance(dataframe_or_series, pd.DataFrame):
-        assert len(dataframe_or_series.columns) > 0
-        return dataframe_or_series.squeeze()
+        if len(dataframe_or_series.columns) != 1:
+            raise ValueError("DataFrame must have exactly one column")
+        if len(dataframe_or_series) == 1:
+            return dataframe_or_series[dataframe_or_series.columns[0]]
+        else:
+            return dataframe_or_series.squeeze()
     elif isinstance(dataframe_or_series, pd.Series):
         return dataframe_or_series
     else:
