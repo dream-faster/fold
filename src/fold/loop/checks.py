2394cbbe50bbb1a7d7a8738679a356b4807e5f9d
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-04-28 17:05:41 +0200

    feature(Loop): added `_train_global()` proof-of-concept function

--- src/fold/loop/checks.py
+++ src/fold/loop/checks.py
@@ -15,3 +15,10 @@ def check_types(
         assert isinstance(X, pd.DataFrame), "X must be a pandas DataFrame."
     assert isinstance(y, pd.Series), "y must be a pandas Series."
     return X, y
+
+
+def check_types_multi_series(data: pd.DataFrame):
+    assert isinstance(data, pd.DataFrame), "data must be a pandas DataFrame."
+    assert "y" in data.columns, "data must have a column `y`."
+    assert "ds" in data.columns, "data must have a column `ds`."
+    assert "unique_id" in data.columns, "data must have a column `unique_id`."
