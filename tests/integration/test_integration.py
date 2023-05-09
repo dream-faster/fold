4a1e75e6bc6e8aebf85bf6b02f177cae637f069e
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-05-09 11:26:54 +0200

    fix(Tests): shorten integration tests

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -10,8 +10,7 @@ from fold.transformations.lags import AddLagsX, AddLagsY
 from fold.utils.dataset import get_preprocessed_dataset
 
 
-@pytest.mark.parametrize("backend", ["no", "ray", "pathos", "thread"])
-def test_on_weather_data(backend: str) -> None:
+def test_on_weather_data() -> None:
     X, y = get_preprocessed_dataset(
         "weather/historical_hourly_la",
         target_col="temperature",
@@ -25,9 +24,24 @@ def test_on_weather_data(backend: str) -> None:
         HistGradientBoostingRegressor(),
     ]
 
-    _, _ = train_backtest(
-        pipeline, X, y, splitter, backend=backend, sample_weights=sample_weights
+    _, _ = train_backtest(pipeline, X, y, splitter, sample_weights=sample_weights)
+
+
+@pytest.mark.parametrize("backend", ["no", "ray", "pathos", "thread"])
+def test_on_weather_data_backends(backend: str) -> None:
+    X, y = get_preprocessed_dataset(
+        "weather/historical_hourly_la",
+        target_col="temperature",
+        shorten=100,
     )
+    splitter = ExpandingWindowSplitter(initial_train_window=0.2, step=0.2)
+    pipeline = [
+        AddLagsX(columns_and_lags=[("pressure", list(range(1, 3)))]),
+        AddLagsY(list(range(1, 10))),
+        HistGradientBoostingRegressor(),
+    ]
+
+    _, _ = train_backtest(pipeline, X, y, splitter, backend=backend)
 
 
 def test_train_evaluate() -> None:
