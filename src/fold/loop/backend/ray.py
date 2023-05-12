16918146658f540e65223be92cc90d5454608837
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-05-12 16:14:00 +0200

    fix(Loop): only paralellize between folds right now (#360)
    * feature(Tests): added failing multiprocessing test
    
    * fix(Loop): only parallelize between folds right now
    
    * fix(Tests): remove pathos

--- src/fold/loop/backend/ray.py
+++ src/fold/loop/backend/ray.py
@@ -53,23 +53,52 @@ def process_child_transformations(
     backend: Backend,
     results_primary: Optional[List[pd.DataFrame]],
 ):
-    func = ray.remote(func)
-    X = ray.put(X)
-    y = ray.put(y)
-    return ray.get(
-        [
-            func.remote(
-                composite,
-                index,
-                child_transformation,
-                X,
-                y,
-                sample_weights,
-                artifacts,
-                stage,
-                backend,
-                results_primary,
-            )
-            for index, child_transformation in list_of_child_transformations_with_index
-        ]
-    )
+    return [
+        func(
+            composite,
+            index,
+            child_transformation,
+            X,
+            y,
+            sample_weights,
+            artifacts,
+            stage,
+            backend,
+            results_primary,
+        )
+        for index, child_transformation in list_of_child_transformations_with_index
+    ]
+
+
+# def process_child_transformations(
+#     func: Callable,
+#     list_of_child_transformations_with_index: List,
+#     composite: Composite,
+#     X: X,
+#     y: Optional[pd.Series],
+#     sample_weights: Optional[pd.Series],
+#     artifacts: Artifact,
+#     stage: Stage,
+#     backend: Backend,
+#     results_primary: Optional[List[pd.DataFrame]],
+# ):
+#     func = ray.remote(func)
+#     X = ray.put(X)
+#     y = ray.put(y)
+#     return ray.get(
+#         [
+#             func.remote(
+#                 composite,
+#                 index,
+#                 child_transformation,
+#                 X,
+#                 y,
+#                 sample_weights,
+#                 artifacts,
+#                 stage,
+#                 backend,
+#                 results_primary,
+#             )
+#             for index, child_transformation in list_of_child_transformations_with_index
+#         ]
+#     )
