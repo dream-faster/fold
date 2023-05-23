ac723a785c95011b2714331faca38c998d6710d4
Daniel Szemerey <account@szemerey.eu>
2023-05-23 13:13:21 +0200

    feature(Utils): `ParsableEnum` now throws back the subclass type (Self) and checks for the `key` rather than the `value` in `from_str`.

--- src/fold/utils/enums.py
+++ src/fold/utils/enums.py
@@ -3,14 +3,16 @@ from __future__ import annotations
 from enum import Enum
 from typing import Union
 
+from typing_extensions import Self
+
 
 class ParsableEnum(Enum):
     @classmethod
-    def from_str(cls, value: Union[str, ParsableEnum]) -> ParsableEnum:
+    def from_str(cls, value: Union[str, ParsableEnum]) -> Self:
         if isinstance(value, cls):
             return value
         for item in cls:
-            if item.value == value:
+            if item.name == value:
                 return item
         else:
             raise ValueError(f"Unknown {cls.__name__}: {value}")
