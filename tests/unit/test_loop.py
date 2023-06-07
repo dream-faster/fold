0670a723849256c35ee8951b228527181088d62b
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-06-07 11:12:23 +0200

    fix: get_acc

--- tests/unit/test_loop.py
+++ tests/unit/test_loop.py
@@ -130,7 +130,7 @@ def test_score_results():
     )
 
     def get_acc(obj):
-        if "accuracy" in obj:
+        if isinstance(obj, "ScoreCard"):
             return obj["accuracy"].result
         else:
             return obj["function"]
