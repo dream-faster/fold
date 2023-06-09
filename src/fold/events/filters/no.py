385a54486f35f56963555129ae40d4d6dd983d2d
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-06-09 17:33:38 +0200

    feature(Events): added FilterZero class (#408)

--- src/fold/events/filters/no.py
+++ src/fold/events/filters/no.py
@@ -6,3 +6,8 @@ from ..base import EventFilter
 class NoFilter(EventFilter):
     def get_event_start_times(self, y: pd.Series) -> pd.DatetimeIndex:
         return y.index
+
+
+class FilterZero(EventFilter):
+    def get_event_start_times(self, y: pd.Series) -> pd.DatetimeIndex:
+        return y[y != 0.0].index
