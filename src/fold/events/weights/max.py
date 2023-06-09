26760c5f2cb3a72561e91b9871914e58ea7dfbb5
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-06-09 16:32:39 +0200

    refactor(Events): calculate_rolling_expanding_abs_max is now a standalone function

--- src/fold/events/weights/max.py
+++ src/fold/events/weights/max.py
@@ -11,18 +11,7 @@ class WeightByMax(WeighingStrategy):
         self.window_size = window_size
 
     def calculate(self, series: pd.Series) -> pd.Series:
-        abs_returns = series.abs()
-        rolling_or_expanding = (
-            abs_returns.expanding()
-            if self.window_size is None
-            else abs_returns.rolling(
-                calculate_rolling_window_size(self.window_size, abs_returns)
-            )
-        )
-        maximum = rolling_or_expanding.max()
-        maximum = maximum.fillna(abs_returns.expanding().max())
-        sample_weights = series.abs() / maximum
-        return sample_weights.fillna(0.0)
+        return calculate_rolling_expanding_abs_max(series, self.window_size)
 
 
 class WeightByMaxWithLookahead(WeighingStrategy):
@@ -30,3 +19,20 @@ class WeightByMaxWithLookahead(WeighingStrategy):
         maximum = series.max()
         sample_weights = series.abs() / maximum
         return sample_weights.fillna(0.0)
+
+
+def calculate_rolling_expanding_abs_max(
+    series: pd.Series, window_size: Optional[Union[int, float]]
+) -> pd.Series:
+    abs_returns = series.abs()
+    rolling_or_expanding = (
+        abs_returns.expanding()
+        if window_size is None
+        else abs_returns.rolling(
+            calculate_rolling_window_size(window_size, abs_returns)
+        )
+    )
+    maximum = rolling_or_expanding.max()
+    maximum = maximum.fillna(abs_returns.expanding().max())
+    sample_weights = series.abs() / maximum
+    return sample_weights.fillna(0.0)
