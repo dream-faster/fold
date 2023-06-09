baf2f650e088a157f357d6aab816392d745806c4
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-06-09 15:52:42 +0200

    fix: types

--- src/fold/events/labeling/strategies.py
+++ src/fold/events/labeling/strategies.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pandas as pd
 
 from ..base import LabelingStrategy
@@ -10,7 +12,7 @@ class BinarizeSign(LabelingStrategy):
         labels.loc[series < 0.0] = 0
         return labels
 
-    def get_all_labels(self) -> list[int]:
+    def get_all_labels(self) -> List[int]:
         return [0, 1]
 
 
@@ -18,5 +20,5 @@ class NoLabel(LabelingStrategy):
     def label(self, series: pd.Series) -> pd.Series:
         return series
 
-    def get_all_labels(self) -> list[int]:
+    def get_all_labels(self) -> List[int]:
         return [0, 1]
