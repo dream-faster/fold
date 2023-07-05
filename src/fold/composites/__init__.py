63e05763110b67ad8a1708f84120f81e819d8236
Daniel Szemerey <account@szemerey.eu>
2023-07-05 15:35:02 +0200

    fix(Linting): Pipeline (Composite) ranamed to `Sequence` to avoid clash with type `Pipeline` and `Pipelines`. (#443)
    * fix(Linting): Defined `Blocks = List[Block]` to be received by `Pipeline`, fixed linting errors.
    
    * fix: No Need for List[Blocks] we were referrencing the wrong type.
    
    * chore
    
    * feature: Renamed `Pipeline` to `Sequence`.
    
    * fix: Export

--- src/fold/composites/__init__.py
+++ src/fold/composites/__init__.py
@@ -4,7 +4,7 @@
 from ..base import Composite
 from .cache import Cache
 from .columns import EnsembleEachColumn, SkipNA, TransformEachColumn
-from .concat import Concat, Pipeline, TransformColumn
+from .concat import Concat, Sequence, TransformColumn
 from .ensemble import Ensemble
 from .imbalance import FindThreshold
 from .metalabeling import MetaLabeling
