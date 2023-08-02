d4dcf972e246ff8a6282c39cb340a57771916a2d
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-08-02 13:04:09 +0200

    feature: adding tests

--- tests/unit/test_events.py
+++ tests/unit/test_events.py
@@ -1,8 +1,10 @@
 import pytest
 from sklearn.ensemble import RandomForestRegressor
 
+from fold.base.classes import PipelineCard
 from fold.events import CreateEvents, FixedForwardHorizon, UsePredefinedEvents
 from fold.events.filters.everynth import EveryNth
+from fold.events.filters.no import NoFilter
 from fold.events.labeling import BinarizeSign
 from fold.events.labeling.strategies import NoLabel
 from fold.events.weights import NoWeighting
@@ -57,9 +59,24 @@ def test_predefined_events(agg_func: str) -> None:
     original_start_times = y.index
     events = labeler.label_events(original_start_times, y).reindex(y.index)
 
-    pipeline = UsePredefinedEvents([AddLagsY([1]), RandomForestRegressor()])
-    pred, trained_pipeline, artifacts = train_backtest(
-        pipeline, X, y, splitter, events=events, return_artifacts=True
+    model = [AddLagsY([1]), RandomForestRegressor(random_state=0)]
+    usepredefined_pipeline = UsePredefinedEvents(model)
+    usepredefined_pred, _, usepredefined_artifact = train_backtest(
+        usepredefined_pipeline, X, y, splitter, events=events, return_artifacts=True
     )
+    assert len(usepredefined_pred) == 1000
+    assert len(usepredefined_pred.dropna()) == 998
+
+    pipeline_card = PipelineCard(
+        preprocessing=None,
+        pipeline=model,
+        event_labeler=labeler,
+        event_filter=NoFilter(),
+    )
+    pred, _, artifact = train_backtest(
+        pipeline_card, X, y, splitter, return_artifacts=True
+    )
+
     assert len(pred) == 1000
     assert len(pred.dropna()) == 998
+    assert pred.equals(usepredefined_pred)
