16f8acac3d359dad84d1aa4f80039e7967c79cdf
Daniel Szemerey <account@szemerey.eu>
2023-08-14 20:28:22 +0200

    feature(Transformation): Adding holiday features based on exchanges. (#511)
    * feature(Transformation): Adding holiday features based on exchanges.
    
    * feature: Support for 'all'
    
    * fix
    
    * feature: Updated dependency
    
    * chore: Updated dependency

--- src/fold/transformations/holidays.py
+++ src/fold/transformations/holidays.py
@@ -72,7 +72,7 @@ class AddHolidayFeatures(Transformation, Tunable):
         self.labeling = LabelingMethod.from_str(labeling)
         from holidays import country_holidays, list_supported_countries
 
-        all_supported_countries = list_supported_countries()
+        all_supported_countries = list(list_supported_countries().keys())
 
         assert all(
             [
@@ -151,6 +151,82 @@ class AddHolidayFeatures(Transformation, Tunable):
     update = fit_noop
 
 
+class AddExchangeHolidayFeatures(Transformation, Tunable):
+    """
+    Adds holiday features for given exchange(s) as new column(s).
+    It uses the pattern "holiday_{exchange}" for naming the columns.
+
+
+    Parameters
+    ----------
+
+    exchange_codes: List[str]
+        List of exchange codes  (eg.: `NYSE`) for which to add holiday features.
+    labeling: LabelingMethod
+        * holiday_binary: Workdays = 0 | National Holidays = 1
+        * weekday_weekend_holiday: Workdays = 0 | Weekends = 1 | National Holidays == 2
+    """
+
+    def __init__(
+        self,
+        exchange_codes: Union[List[str], str],
+        labeling: Union[str, LabelingMethod] = LabelingMethod.weekday_weekend_holiday,
+        keep_original: bool = True,
+        name: Optional[str] = None,
+        params_to_try: Optional[dict] = None,
+    ) -> None:
+        self.exchange_codes = wrap_in_list(exchange_codes)
+        self.labeling = LabelingMethod.from_str(labeling)
+        import pandas_market_calendars as mcal
+
+        all_supported_exchanges = mcal.get_calendar_names()
+
+        if "all" in self.exchange_codes:
+            self.exchange_codes = all_supported_exchanges
+
+        assert self.labeling in [
+            LabelingMethod.holiday_binary,
+            LabelingMethod.weekday_weekend_holiday,
+        ], "Only holiday_binary and weekday_weekend_holiday are supported for stockexchanges"
+        assert all(
+            [
+                country_code in all_supported_exchanges
+                for country_code in self.exchange_codes
+            ]
+        ), f"Exchange code not supported: {exchange_codes}"
+
+        self.params_to_try = params_to_try
+        self.keep_original = keep_original
+        self.name = name or f"AddExchangeHolidayFeatures-{self.exchange_codes}"
+        self.properties = Transformation.Properties(requires_X=False)
+
+    def transform(self, X: pd.DataFrame, in_sample: bool) -> pd.DataFrame:
+        if self.labeling == LabelingMethod.holiday_binary:
+            holidays = _get_exchange_holidays(X.index, self.exchange_codes)
+            holidays[holidays != 0] = 1
+
+        elif self.labeling == LabelingMethod.weekday_weekend_holiday:
+            holidays = _get_exchange_holidays(X.index, self.exchange_codes)
+
+            # All values that are (bigger than 0 or a string) are holidays, but we don't want to differentiate between them
+            holidays[holidays != 0] = 2
+
+            holidays[holidays == 0] = holidays[holidays == 0].add(
+                _get_weekends(X.index), axis="index"
+            )
+
+        else:
+            raise ValueError(f"Unknown HolidayType: {self.labeling}")
+
+        concatenated = (
+            pd.concat([X, holidays], axis="columns") if self.keep_original else holidays
+        )
+        return concatenated
+
+    fit = fit_noop
+    update = fit_noop
+
+
 def _get_weekends(dates: pd.DatetimeIndex) -> pd.Series:
     return pd.Series((dates.weekday > 4).astype(int), index=dates)
 
@@ -188,3 +264,23 @@ def _get_holidays(
             )
 
     return df
+
+
+def _get_exchange_holidays(
+    dates: pd.DatetimeIndex, exchange_codes: List[str]
+) -> pd.DataFrame:
+    import pandas_market_calendars as mcal
+
+    return pd.concat(
+        [
+            mcal.get_calendar(exchange)
+            .schedule(start_date=dates[0], end_date=dates[-1])
+            .iloc[:, 0]
+            .reindex(pd.date_range(dates[0], dates[-1]))
+            .isna()
+            .astype(int)
+            .rename(f"holiday_{exchange}")
+            for exchange in exchange_codes
+        ],
+        axis="columns",
+    )
