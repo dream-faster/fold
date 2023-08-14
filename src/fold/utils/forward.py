c339f5a1da8ea05ba9d929b9cb9fe42a3f96e83d
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-08-14 13:12:03 +0200

    fix(Labeling): apply `transformation_func` properly

--- src/fold/utils/forward.py
+++ src/fold/utils/forward.py
@@ -14,4 +14,4 @@ def create_forward_rolling(
     indexer = pd.api.indexers.FixedForwardWindowIndexer(window_size=period)
     shift_by = shift_by if shift_by is not None else -int(max(1, period / 2))
     transformation_func = transformation_func if transformation_func else lambda x: x
-    return agg_func(transformation_func(series.rolling(window=indexer))).shift(shift_by)
+    return agg_func(transformation_func(series).rolling(window=indexer)).shift(shift_by)
